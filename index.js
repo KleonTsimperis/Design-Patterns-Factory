7e8613dd0a4d6aac8a19949011eacedbc74571e5
KleonTsimperis <30871407+KleonTsimperis@users.noreply.github.com>
2022-12-01 11:44:05 +0100

    1 file modified

--- index.js
+++ index.js
@@ -5,10 +5,24 @@
  *
  */
 
-
 class IOSButton {}
-
 class AndroidButton {}
 
-const ios = new IOSButton();
-console.log(ios, 2);
+// Without Factory - using a bunch of conditional checking and new keywords, hard to maintain
+const button1 = (os) => (os === 'ios' ? new IOSButton() : new AndroidButton());
+const button2 = (os) => (os === 'ios' ? new IOSButton() : new AndroidButton());
+
+// Instead create a subclass or function to determine which object to instantiate
+class ButtonFactory {
+  createButton(os) {
+    if (os === 'ios') {
+      return new IOSButton();
+    } else {
+      return new AndroidButton();
+    }
+  }
+}
+
+// With Factory
+const factory = new ButtonFactory();
+const bt1 = factory.createButton('ios');
